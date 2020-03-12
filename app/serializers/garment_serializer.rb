0b7fd82d3c72932ffd751e08bac753248fc4f678
lindsay-kaufman <linds234@gmail.com>
2020-03-11 21:40:45 -0400

    Create custom attribute in Garment serializer
    Created a weighted method to return weighted sum of textile percentages
    - Next step is adding form to get percentages on the client side

--- app/serializers/garment_serializer.rb
+++ app/serializers/garment_serializer.rb
@@ -1,5 +1,5 @@
 class GarmentSerializer < ActiveModel::Serializer
-  attributes :id, :style, :description, :rating, :editable, :textiles
+  attributes :id, :style, :description, :rating, :textiles, :weighted, :editable
   has_one :user
   has_many :components
   has_many :textiles
@@ -12,4 +12,19 @@ class GarmentSerializer < ActiveModel::Serializer
     object.textiles
   end
 
+  def weighted
+    weightedScores = Array.new
+    object.textiles.each do |textile| 
+      object.components.each do |component|
+        if component.percent != nil && textile.id == component.textile_id
+          tmp = textile.impact_score * component.percent
+          score = tmp / 100
+          weightedScores.push(score)
+        end
+      end
+    end
+    weightedScores.reduce(:+)
+  end
+
 end
+
